2a86b79e6abda742485dc947c24cf1181bdcade9
Brijesh B S <brijeshbs007@gmail.com>
2020-12-21 19:07:50 +0530

    changing files

--- JavaProject/src/chatting/application/package-info.java
+++
@@ -1,1 +0,0 @@
-package chatting.application;
--- The Endless Chat/Servers.java
+++ The Endless Chat/Servers.java
@@ -1,4 +1,4 @@
-package chatting.application;
+package ChattingApplication;
 
 import javax.swing.*;
 import javax.swing.border.*;
@@ -7,10 +7,13 @@ import java.awt.event.*;
 import java.net.*;
 import java.io.*;
 
+import javax.swing.plaf.*;
+import javax.swing.plaf.basic.*;
+
 import java.util.Calendar;
 import java.text.SimpleDateFormat;
 
-public class Server implements ActionListener{
+public class Servers implements ActionListener{
     
     JPanel p1;
     JTextField t1;
@@ -27,15 +30,15 @@ public class Server implements ActionListener{
     
     Boolean typing;
     
-    Server(){
+    Servers(){
         f1.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         p1 = new JPanel();
         p1.setLayout(null);
-        p1.setBackground(new Color(2, 119, 189));
+        p1.setBackground(new Color(7, 94, 84));
         p1.setBounds(0, 0, 450, 70);
         f1.add(p1);
         
-       ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("chatting/application/icons/3.png"));
+       ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("ChattingApplication/3.png"));
        Image i2 = i1.getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT);
        ImageIcon i3 = new ImageIcon(i2);
        JLabel l1 = new JLabel(i3);
@@ -48,46 +51,46 @@ public class Server implements ActionListener{
            }
        });
        
-       ImageIcon i4 = new ImageIcon(ClassLoader.getSystemResource("chatting/application/icons/bond.jpg"));
-       Image i5 = i4.getImage().getScaledInstance(60, 60, Image.SCALE_DEFAULT);
+       ImageIcon i4 = new ImageIcon(ClassLoader.getSystemResource("ChattingApplication/Harvey Spectre.jpg"));
+       Image i5 = i4.getImage().getScaledInstance(63, 63, Image.SCALE_DEFAULT);
        ImageIcon i6 = new ImageIcon(i5);
        JLabel l2 = new JLabel(i6);
-       l2.setBounds(40, 5, 60, 60);
+       l2.setBounds(40, 5, 63, 63);
        p1.add(l2);
        
-       ImageIcon i7 = new ImageIcon(ClassLoader.getSystemResource("chatting/application/icons/video.png"));
+       ImageIcon i7 = new ImageIcon(ClassLoader.getSystemResource("ChattingApplication/video.png"));
        Image i8 = i7.getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT);
        ImageIcon i9 = new ImageIcon(i8);
        JLabel l5 = new JLabel(i9);
-       l5.setBounds(290, 20, 30, 30);
+       l5.setBounds(330,15, 30, 30);
        p1.add(l5);
        
-       ImageIcon i11 = new ImageIcon(ClassLoader.getSystemResource("chatting/application/icons/phone.png"));
-       Image i12 = i11.getImage().getScaledInstance(35, 30, Image.SCALE_DEFAULT);
+       ImageIcon i11 = new ImageIcon(ClassLoader.getSystemResource("ChattingApplication/phone.png"));
+       Image i12 = i11.getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT);
        ImageIcon i13 = new ImageIcon(i12);
        JLabel l6 = new JLabel(i13);
-       l6.setBounds(350, 20, 35, 30);
+       l6.setBounds(380, 15, 30, 30);
        p1.add(l6);
        
-       ImageIcon i14 = new ImageIcon(ClassLoader.getSystemResource("chatting/application/icons/3icon.png"));
-       Image i15 = i14.getImage().getScaledInstance(13, 25, Image.SCALE_DEFAULT);
+       ImageIcon i14 = new ImageIcon(ClassLoader.getSystemResource("ChattingApplication/3icon.png"));
+       Image i15 = i14.getImage().getScaledInstance(10, 25, Image.SCALE_DEFAULT);
        ImageIcon i16 = new ImageIcon(i15);
        JLabel l7 = new JLabel(i16);
-       l7.setBounds(410, 20, 13, 25);
+       l7.setBounds(423, 17, 10, 25);
        p1.add(l7);
        
        
-       JLabel l3 = new JLabel("James Bond");
-       l3.setFont(new Font("MAGNETO", Font.BOLD, 18));
+       JLabel l3 = new JLabel("Harvey Spectre");
+       l3.setFont(new Font("SAN_SERIF", Font.BOLD, 15));
        l3.setForeground(Color.WHITE);
-       l3.setBounds(110, 15, 120, 18);
+       l3.setBounds(110, 15, 120, 15);
        p1.add(l3);   
        
        
        JLabel l4 = new JLabel("Active Now");
-       l4.setFont(new Font("f", Font.PLAIN, 14));
+       l4.setFont(new Font("SAN_SERIF", Font.PLAIN, 10));
        l4.setForeground(Color.WHITE);
-       l4.setBounds(110, 35, 100, 20);
+       l4.setBounds(110, 35, 112, 15);
        p1.add(l4);  
        
        Timer t = new Timer(1, new ActionListener(){
@@ -102,14 +105,41 @@ public class Server implements ActionListener{
        
        
        a1 = new JPanel();
-       a1.setBounds(5, 75, 440, 570);
-       a1.setFont(new Font("CONSOLAS", Font.PLAIN, 16));
-       f1.add(a1);
+       //a1.setBounds(5, 75, 440, 358);
+       a1.setFont(new Font("SAN_SERIF", Font.PLAIN, 17));
+       //f1.add(a1);
        
+       JScrollPane sp = new JScrollPane(a1);
+       sp.setBounds(5, 75, 440, 358);
+       sp.setBorder(BorderFactory.createEmptyBorder());
+       //f1.add(sp);
        
+       ScrollBarUI ui = new BasicScrollBarUI()
+       {
+    	   protected JButton createDecreaseButton(int orientation)
+    	   {
+    	   JButton button = super.createDecreaseButton(orientation);
+    	   button.setBackground(new Color(7,94,84));
+    	   button.setForeground(Color.WHITE);
+    	   this.thumbColor = new Color(7,94,84);
+    	   return button;
+    	   }
+       
+       protected JButton createIncreaseButton(int orientation)
+       {
+	   JButton button = super.createIncreaseButton(orientation);
+	   button.setBackground(new Color(7,94,84));
+	   button.setForeground(Color.WHITE);
+	   this.thumbColor = new Color(7,94,84);
+	   return button;
+   }
+};
+        sp.getVerticalScrollBar().setUI(ui);
+        f1.add(sp);
+        
        t1 = new JTextField();
-       t1.setBounds(5, 655, 310, 40);
-       t1.setFont(new Font("CONSOLAS", Font.PLAIN, 16));
+       t1.setBounds(3, 439, 344, 40);
+       t1.setFont(new Font("SAN_SERIF", Font.PLAIN, 16));
        f1.add(t1);
        
        t1.addKeyListener(new KeyAdapter(){
@@ -131,17 +161,17 @@ public class Server implements ActionListener{
        });
        
        b1 = new JButton("Send");
-       b1.setBounds(320, 655, 123, 40);
-       b1.setBackground(new Color(209, 237, 242));
-       b1.setForeground(Color.BLACK);
-       b1.setFont(new Font("CONSOLAS", Font.BOLD, 20));
+       b1.setBounds(348,439,100,40);
+       b1.setBackground(new Color(7, 94, 84));
+       b1.setForeground(Color.WHITE);
+       b1.setFont(new Font("SAN_SERIF", Font.PLAIN, 17));
        b1.addActionListener(this);
        f1.add(b1);
         
-       f1.getContentPane().setBackground(new Color(2, 119, 189));
+       f1.getContentPane().setBackground(Color.WHITE);
        f1.setLayout(null);
        f1.setSize(450, 700);
-       f1.setLocation(200, 20); 
+       f1.setLocation(300, 200); 
        f1.setUndecorated(true);
        f1.setVisible(true);
         
@@ -174,11 +204,11 @@ public class Server implements ActionListener{
         JPanel p3 = new JPanel();
         p3.setLayout(new BoxLayout(p3, BoxLayout.Y_AXIS));
         
-        JLabel l1 = new JLabel("<html><p style = \"width : 150px\">"+out+"</p></html>");
+        JLabel l1 = new JLabel("<html><p style = \"width : 75px\">"+out+"</p></html>");
         l1.setFont(new Font("Tahoma", Font.PLAIN, 16));
         l1.setBackground(new Color(37, 211, 102));
         l1.setOpaque(true);
-        l1.setBorder(new EmptyBorder(15,15,15,50));
+        l1.setBorder(new EmptyBorder(5,5,5,10));
         
         Calendar cal = Calendar.getInstance();
         SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
@@ -192,7 +222,7 @@ public class Server implements ActionListener{
     }
     
     public static void main(String[] args){
-        new Server().f1.setVisible(true);
+        new Servers().f1.setVisible(true);
         
         String msginput = "";
         try{
@@ -217,3 +247,5 @@ public class Server implements ActionListener{
         }catch(Exception e){}
     }    
 }
+
+
